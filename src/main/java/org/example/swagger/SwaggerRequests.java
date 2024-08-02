d3937148dc686c5304be63275a419f0ae91e4e17
Begum Ugurlu <begum.ugurlu@tufts.edu>
2024-08-02 15:33:38 +0300

    Initial commit

---
+++ src/main/java/org/example/swagger/SwaggerRequests.java
@@ -0,0 +1,13 @@
+package org.example.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SwaggerRequests {
+
+    public static void main (String[] args) {
+
+    }
+
+
+}
